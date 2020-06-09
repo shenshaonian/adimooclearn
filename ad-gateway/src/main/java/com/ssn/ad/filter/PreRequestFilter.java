654cd80acd02a6b62ae289d059df5546322b7f0f
shenshaonian <1343513404@qq.com>
2020-06-10 00:59:36 +0800

    3-8 zuul简单的过滤器的介绍和使用

---
+++ ad-gateway/src/main/java/com/ssn/ad/filter/PreRequestFilter.java
@@ -0,0 +1,38 @@
+package com.ssn.ad.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description:
+ * @author: 范子祺
+ **/
+@Slf4j
+@Component
+public class PreRequestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        context.set("startTime",System.currentTimeMillis());
+        return null;
+    }
+}
